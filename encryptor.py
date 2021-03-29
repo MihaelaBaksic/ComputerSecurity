11b1a6f2134455478d7eb50b758963acfdec307e
MihaelaBaksic <mihaela.baksic@gmail.com>
2021-03-29 18:09:54 +0200

    Compact storing

--- encryptor.py
+++ encryptor.py
@@ -2,7 +2,6 @@ from Crypto.Cipher import AES
 import pbkd
 
 
-# todo store both ct and tag
 def encrypt(master_password: str, pt: bytes):
     key = pbkd.derive_for_encryption(master_password)
 
@@ -10,41 +9,43 @@ def encrypt(master_password: str, pt: bytes):
 
     ct, tag = aes.encrypt_and_digest(pt)
 
-    f = open('nonce.in', 'wb+')
+    f = open('db.bin', 'wb+')
     f.write(aes.nonce)
+    f.write(tag)
+    f.write(ct)
     f.close
 
-    f = open('tag.in', 'wb+')
-    f.write(tag)
-    f.close()
+    # f = open('tag.in', 'wb+')
+    # f.write(tag)
+    # f.close()
 
-    f = open('ciphertext.in', 'wb+')
-    f.write(ct)
-    f.close()
+    # f = open('ciphertext.in', 'wb+')
+    # f.write(ct)
+    # f.close()
 
 
 def decrypt(master_password: str):
     key = pbkd.derive_for_decryption(master_password)
 
-    f = open('nonce.in', 'rb')
-    nonce = f.read()
-    f.close()
-
-    f = open('tag.in', 'rb')
-    tag = f.read()
-    f.close()
-
-    f = open('ciphertext.in', 'rb')
+    f = open('db.bin', 'rb')
+    nonce = f.read(16)
+    tag = f.read(16)
     ct = f.read()
     f.close()
+
+    # f = open('tag.in', 'rb')
+    # tag = f.read()
+    # f.close()
+
+    # f = open('ciphertext.in', 'rb')
+    # ct = f.read()
+    # f.close()
 
     aes = AES.new(key, mode=AES.MODE_EAX, nonce=nonce)
     pt = aes.decrypt(ct)
 
     aes.verify(tag)
     return pt
-
-
 
 # encrypt('masterPass', b'plaintext'))
 # decrypt('masterPass')
